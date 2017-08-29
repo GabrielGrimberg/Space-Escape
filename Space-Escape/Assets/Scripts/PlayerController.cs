4f052f297fd19e22596519ca100ab2aa6679b25a
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-08-29 16:57:40 +0100

    Menu and Player Added

---
+++ Space-Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 15.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.LeftArrow) )
+		{
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}
+		
+	}
+}
