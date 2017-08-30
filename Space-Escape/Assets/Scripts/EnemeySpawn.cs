357960d02bf1e727f0c3d7cb0b75365e117f64e7
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-08-30 14:02:02 +0100

    Player Movement Fix & Enemy Spawn

---
+++ Space-Escape/Assets/Scripts/EnemeySpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeySpawn : MonoBehaviour 
+{
+	public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
--- Space-Escape/Assets/Scripts/PlayerController.cs
+++ Space-Escape/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,23 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed = 15.0f;
 
+	//Fixes the ship movement display.
+	public float padding = 1f;
+
+	float xMin;
+	float xMax;
+
 	// Use this for initialization
 	void Start () 
 	{
-		
+		float distance = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance) );
+
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance) );
+
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
 	}
 	
 	// Update is called once per frame
@@ -17,12 +30,18 @@ public class PlayerController : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.LeftArrow) )
 		{
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			//transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			//transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		//Restrict the player to the gamespace.
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 		
 	}
 }
