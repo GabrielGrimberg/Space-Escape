309f7a33546de0f004f17f8d340570034540d75d
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-08-31 14:51:12 +0100

    Enemy Contact

---
+++ Space-Escape/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour 
+{
+	public float health = 150;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		//Debug.Log(collider);
+		Laser missile = collider.gameObject.GetComponent<Laser>();
+
+		if(missile)
+		{
+			health -= missile.getDamage();
+
+			missile.Hit();
+
+			if(health <= 0)
+			{
+				Destroy(gameObject);
+			}
+			//Debug.Log("Hit by a laser");
+
+		}
+	}
+}
---
+++ Space-Escape/Assets/Scripts/Laser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour 
+{
+	public float damage = 100f;
+
+	public float getDamage()
+	{
+		return damage;
+	}
+
+	public void Hit()
+	{
+		Destroy(gameObject);
+	}
+}
