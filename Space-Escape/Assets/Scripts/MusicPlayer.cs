50af63575e7ca3d2b62ceb4d05a23e381d0e91e5
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-09-04 17:33:09 +0100

    Menu Final Update

--- Space-Escape/Assets/Scripts/MusicPlayer.cs
+++ Space-Escape/Assets/Scripts/MusicPlayer.cs
@@ -1,17 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MusicPlayer : MonoBehaviour {
+public class MusicPlayer : MonoBehaviour 
+{
 	static MusicPlayer instance = null;
-	
-	void Start () {
-		if (instance != null && instance != this) {
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	private AudioSource music;
+
+	void Start () 
+	{
+		if (instance != null && instance != this) 
+		{
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
-		} else {
+		} 
+		else 
+		{
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
 		}
 		
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		music.Stop();
+
+		if(level == 0)
+		{
+			music.clip = startClip;
+		}
+		if(level == 1)
+		{
+			music.clip = gameClip;
+		}
+		if(level == 2)
+		{
+			music.clip = endClip;
+		}
+
+		music.loop = true;
+		music.Play();
 	}
 }
